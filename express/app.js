1b5bc12eda77feccc25fdf41398df3cf7dd9c41d
Baek Gi Chan <laphayen@gmail.com>
2022-09-09 19:29:07 +0900

    Upload jade

--- express/app.js
+++ express/app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'jade');
+app.set('views', './views');
 app.use(express.static('public'));
 app.get('/', function(req, res){
 	res.send('Hello Home Page');
