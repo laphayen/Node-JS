c214dee497deb7841b840582b7f98a2ec2f10ba1
Baek Gi Chan <laphayen@gmail.com>
2022-09-11 23:09:15 +0900

    Update app_file.js

--- express/app_file.js
+++ express/app_file.js
@@ -2,24 +2,52 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(urlencodedParser = bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
 app.set('views', './views_file');
 app.set('view engine', 'jade');
 app.get('/topic/new', function(req, res){
   res.render('new');
 });
-app.post('/topic/new', function(req, res){
+app.post('/topic', function(req, res){
   var title = req.body.title;
   var description = req.body.description;
-  fs.writeFile('data/'+title, description, function(err){
-    if(err){
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-    res.send('Success');
-  });
+  res.send('HI, port, '+req.body.title);
+//  fs.writeFile('data/'+title, description, function(err){
+//      console.log(err);
+//      res.status(500).send('Internal Server Error');
+//    }
+//    res.send('Success');
+//  });
 });
 app.listen(3000, function(){
   console.log('Conneted, 3000 port');
 });
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var fs = require('fs');
+// var app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.locals.pretty = true;
+// app.set('views', './views_file');
+// app.set('view engine', 'jade');
+// app.get('/topic/new', function(req, res){
+//   res.render('new');
+// });
+// app.post('/topic', function(req, res){
+//   res.send('hi, post');
+  //
+  // var title = req.body.title;
+  // var description = req.body.description;
+  // fs.writeFile('data/'+title, description, function(err){
+  //   if(err){
+  //     console.log(err);
+  //     res.status(500).send('Internal Server Error');
+  //   };
+  //   res.send('Success');
+  // });
+// });
+// app.listen(3000, function(){
+//   console.log('Conneted, 3000 port');
+// });
